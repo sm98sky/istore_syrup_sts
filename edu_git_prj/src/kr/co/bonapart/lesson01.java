816d2a2e8b305aaf0d015a954835f0f4d64d47a6
sm98sky <sm98sky@naver.com>
2015-10-30 14:27:30 +0900

    1

--- edu_git_prj/src/kr/co/bonapart/lesson01.java
+++ edu_git_prj/src/kr/co/bonapart/lesson01.java
@@ -4,7 +4,7 @@ public class lesson01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Start !! ");
+		System.out.println("Start !! ~~");
 	}
 
 }
